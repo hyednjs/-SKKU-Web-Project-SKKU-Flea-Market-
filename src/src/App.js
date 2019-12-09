4893fef331e8c37fc9f2134fd8e2abaf6e010093
joohw1115 <joohw1115@naver.com>
2019-12-09 17:24:27 +0900

    Login / Register Page

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Login from './Login';
 
 function App() {
--- src/src/Login.js
+++ src/src/Login.js
@@ -1,6 +1,8 @@
-/*global chrome*/
 import React, { useState } from 'react';
 import './Login.css';
+import jQuery from "jquery";
+
+window.$ = window.jQuery = jQuery;
 
 function HelpMessage(props) {
   function onScroller() {
@@ -17,31 +19,23 @@ function HelpMessage(props) {
   );
 }
 
-function Login(props) {
+function Login() {
 
   const [id, setID] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
-
-  function loginHandler() {
-
-  }
-
-  function registerHandler() {
-  }
-
   return (
     <div class="login-page">
-      <img class="logo-img" src="img/logo.png" />
+      <img class="logo-img" src="img/flea_market.png" />
       <div class="form">
 
         <form class="register-form">
           <h2>REGISTER</h2>
-          <input type="text" placeholder="name" name="id" required uired onChange={(e) => setID(e.target.value)} />
+          <input type="text" placeholder="name" name="id" required onChange={(e) => setID(e.target.value)} />
           <input type="password" placeholder="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
           <input type="text" placeholder="email address" name="email" required onChange={(e) => setEmail(e.target.value)} />
-          <button class="create-btn" type="reset" onClick={registerHandler}>create</button>
+          <button class="create-btn" type="reset">create</button>
           <HelpMessage msg="Already registered? " detail="Sign In" />
         </form>
 
@@ -49,7 +43,7 @@ function Login(props) {
           <h2>LOGIN</h2>
           <input type="text" placeholder="username" onChange={(e) => setID(e.target.value)} />
           <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-          <button class="login-btn" type="reset" onClick={loginHandler}>login</button>
+          <button class="login-btn" type="reset">login</button>
           <HelpMessage msg="Not registered? " detail="Create an account" />
         </form>
       </div>
